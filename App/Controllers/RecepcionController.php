f4293e272ae23e7d3a980e32d87dbccfc448fc3d
Ferna-Code <jackluengo@gmail.com>
2024-01-26 20:47:27 -0300

    Update RecepcionController.php
    Mi nuevo cambio

--- App/Controllers/RecepcionController.php
+++ App/Controllers/RecepcionController.php
@@ -5,7 +5,7 @@ class RecepcionController
 {
     public function index()
     {
-
+//Aqui voy
         if (isset($_SESSION['nivelUsuario']) && $_SESSION['nivelUsuario'] == 2) {
             require_once VIEWS_PATH . 'layout/header.php';
             include VIEWS_PATH . "recepcion/index.php";
