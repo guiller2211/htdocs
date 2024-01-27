c5581499708b9bbcd8c50bee7fcc8e4a806e2bcd
Fernando Reyes <144495054+Ferna-Code@users.noreply.github.com>
2024-01-27 16:18:25 -0300

    Feature/recepcion f (#10)
    * elimine mensaje
    
    eliminar mensaje
    
    * otra
    
    ---------
    
    Co-authored-by: Ferna-Code <jackluengo@gmail.com>

--- App/Controllers/RecepcionController.php
+++ App/Controllers/RecepcionController.php
@@ -5,7 +5,8 @@ class RecepcionController
 {
     public function index()
     {
-        //aca estuvo Fernando
+        //aca estuve otra ves
+        
         if (isset($_SESSION['nivelUsuario']) && $_SESSION['nivelUsuario'] == 2) {
             require_once VIEWS_PATH . 'layout/header.php';
             include VIEWS_PATH . "recepcion/index.php";
