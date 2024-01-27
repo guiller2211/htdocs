01ce41226d8b7a291127f06d203aff1b7bda8f24
Richard Portatil <rcalderon2.0pro@gmail.com>
2024-01-27 09:56:47 -0300

    prueba commit richard

--- App/Controllers/RecepcionController.php
+++ App/Controllers/RecepcionController.php
@@ -10,7 +10,6 @@ class RecepcionController
             require_once VIEWS_PATH . 'layout/header.php';
             include VIEWS_PATH . "recepcion/index.php";
             require_once VIEWS_PATH . 'layout/footer.php';
-            echo "Punto de control";
         } else {
             require_once VIEWS_PATH . 'layout/header.php';
             include VIEWS_PATH . 'error/index.php';
