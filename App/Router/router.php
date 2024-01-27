181e32fa982b6f3c6db3f093a101a7dd81f9e1e9
guiller2211 <72982551+guiller2211@users.noreply.github.com>
2024-01-27 12:07:08 -0300

    quitando comentario

--- App/Router/router.php
+++ App/Router/router.php
@@ -3,6 +3,14 @@ class Router
 {
     public static function route()
     {
+        // Definir el contexto de base
+        $base_path = '/ipleones/labMuest';
+
+        // Asegúrate de que la URL comience con "/labMuest/"
+        if (strpos($_SERVER['REQUEST_URI'], $base_path) === 0) {
+            $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strlen($base_path));
+        }
+
         if ($_SERVER['REQUEST_URI'] == '/' || $_SERVER['REQUEST_URI'] == '') {
             $controller = "Home";
             $method = "index";
@@ -16,7 +24,7 @@ class Router
 
         // Cargar la clase y el método que nos piden
         $controller_file = CONTROLLERS_PATH . $controller . 'Controller' . '.php';
-        
+
         if (file_exists($controller_file)) {
             require_once $controller_file;
             $const = $controller . 'Controller';
