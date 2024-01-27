414bf6e8af8d8d508f2eeb6a9c2ca5ca247088cd
Fernando Reyes <144495054+Ferna-Code@users.noreply.github.com>
2024-01-27 16:19:09 -0300

    Feature/recepcion f (#11)
    * Update RecepcionController.php
    
    *Mi nuevo cambio
    
    * recepcionController
    
    *Yo estuve aqui
    
    * primer commit
    
    *aca estuvo Fernando
    
    * elimine mensaje
    
    eliminar mensaje
    
    * otra
    
    ---------
    
    Co-authored-by: Ferna-Code <jackluengo@gmail.com>

--- App/Controllers/RecepcionController.php
+++ App/Controllers/RecepcionController.php
@@ -5,11 +5,13 @@ class RecepcionController
 {
     public function index()
     {
-
+        //aca estuve otra ves
+        
         if (isset($_SESSION['nivelUsuario']) && $_SESSION['nivelUsuario'] == 2) {
             require_once VIEWS_PATH . 'layout/header.php';
             include VIEWS_PATH . "recepcion/index.php";
             require_once VIEWS_PATH . 'layout/footer.php';
+            echo "Punto de control";
         } else {
             require_once VIEWS_PATH . 'layout/header.php';
             include VIEWS_PATH . 'error/index.php';
