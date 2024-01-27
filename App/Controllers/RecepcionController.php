b5f806f1d580700ef1a9db80031355573f395aec
Ferna-Code <jackluengo@gmail.com>
2024-01-27 16:22:42 -0300

    elimine espacios

--- App/Controllers/RecepcionController.php
+++ App/Controllers/RecepcionController.php
@@ -4,9 +4,7 @@ session_start();
 class RecepcionController
 {
     public function index()
-    {
-       
-
+    {      
         if (isset($_SESSION['nivelUsuario']) && $_SESSION['nivelUsuario'] == 2) {
             require_once VIEWS_PATH . 'layout/header.php';
             include VIEWS_PATH . "recepcion/index.php";
