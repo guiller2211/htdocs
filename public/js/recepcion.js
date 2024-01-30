0077da0e82ee3ccb957b858fe8061d1afcbf58ac
Guillermo Araujo <guillermo.araujo@smithcommerce.com>
2024-01-29 22:33:42 -0300

    agregando js

--- public/js/recepcion.js
+++ public/js/recepcion.js
@@ -1,3 +1,87 @@
+getUsuarios();
+//PACIENTE
+$("#formRegistro").on("submit", function (event) {
+  event.preventDefault();
+
+  var formData = {
+    rut: $("#rut1").val(),
+    nombre: $("#nombre1").val(),
+    apPat: $("#apPat1").val(),
+    apMat: $("#apMat1").val(),
+    telefono: $("#telefono1").val(),
+    direccion: $("#direccion1").val(),
+    mail: $("#mail1").val(),
+    Fnac: $("#Fnac1").val(),
+    genero: $("#genero1").val(),
+  };
+
+  fetch("Recepcion/insertUser", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((data) => {
+      if (!data.success) {
+        alert("¡Datos agregados!");
+        // Resto del código para manejar la respuesta exitosa
+        $(
+          "#rut, #nombre, #apPat, #apMat, #telefono, #direccion, #mail, #Fnac, #genero"
+        ).val("");
+      } else {
+        alert("Error" + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error en la solicitud Fetch: ", error);
+    });
+});
+
+//EXAMEN
+$("#formExamen").on("submit", function (event) {
+  event.preventDefault();
+  var formData = {
+    paciente_id: $("#rut").find(":selected").data("test"),
+    fecha: $("#fecha").val(),
+    centroCodigo: $("#opciones").val(),
+  };
+
+  fetch("recepcion/insertExamen", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((data) => {
+      if (!data.success) {
+        alert("¡Datos agregados!");
+        // Resto del código para manejar la respuesta exitosa
+        $(
+          "#rut, #nombre, #apPat, #apMat, #telefono, #direccion, #mail, #fecha, #opciones"
+        ).val("");
+      } else {
+        alert("Error" + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error en la solicitud Fetch: ", error);
+    });
+});
+
 $("#buscar").on("change", function (event) {
   event.preventDefault();
   var formData = {
@@ -23,26 +107,100 @@ $("#buscar").on("change", function (event) {
       console.error("Error en la solicitud Fetch: ", error);
     });
 });
-function actualizarSelect(data) { // Fun actualizar el select con los datos obtenidos************
+
+function actualizarSelect(data) {
+  // Fun actualizar el select con los datos obtenidos************
   const select = $("select");
-  select.empty();  // clear antes de actualizar
+  select.empty(); // clear antes de actualizar
 
   $.each(data, function (i, row) {
-    const option = $(`<option data-test="${row.rut}" class="obtenerID">${row.rut}</option>`);
+    const option = $(
+      `<option data-test="${row.rut}" class="obtenerID">${row.rut}</option>`
+    );
     select.append(option);
   });
 }
 
+function getUsuarios() {
+  fetch("recepcion/getData")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        const select = $("#rut");
+        select.empty(); // clear antes de actualizar
+        const optionSelect = $(
+          `<option class="obtenerID">Seleccione Paciente</option>`
+        );
+        select.append(optionSelect);
+        $.each(data, function (i, row) {
+          const option = $(
+            `<option data-test="${row.id}" class="obtenerID">${row.rut}</option>`
+          );
+          select.append(option);
+        });
+      } else {
+        alert("Error en la actualización: " + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error en la solicitud Fetch: ", error);
+    });
+}
+
+$("#rut").on("change", function (event) {
+  event.preventDefault();
+  var formData = {
+    rut: $(this).val(),
+  };
+
+  fetch("recepcion/buscarRut", {
+    method: "POST",
+    body: JSON.stringify(formData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        $("#nombre").val(data[0].nombre);
+        $("#apPat").val(data[0].apPat);
+        $("#apMat").val(data[0].apMat);
+        $("#telefono").val(data[0].telefono);
+        $("#direccion").val(data[0].direccion);
+        $("#mail").val(data[0].mail);
+      } else {
+        alert("Error en la actualización: " + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error en la solicitud Fetch: ", error);
+    });
+});
 
 function controlVisi1() {
-  var elemento2 = document.getElementById('ingreso');
-  elemento.style.display = 'none';
-  elemento2.style.display = 'block';
+  var elemento = document.getElementById("ingreso");
+  var elemento2 = document.getElementById("diagnostico");
+  var elemento3 = document.getElementById("ingresarPaciente");
+  elemento.style.display = "block";
+  elemento2.style.display = "none";
+  elemento3.style.display = "none";
 }
 
 function controlVisi2() {
-  var elemento = document.getElementById('ingreso');
-  var elemento2 = document.getElementById('diagnostico');
-  elemento.style.display = 'none';
-  elemento2.style.display = 'block';
+  var elemento = document.getElementById("ingreso");
+  var elemento2 = document.getElementById("diagnostico");
+  var elemento3 = document.getElementById("ingresarPaciente");
+  elemento.style.display = "none";
+  elemento2.style.display = "block";
+  elemento3.style.display = "none";
+}
+
+function controlVisi3() {
+  var elemento = document.getElementById("ingresarPaciente");
+  var elemento2 = document.getElementById("diagnostico");
+  var elemento3 = document.getElementById("ingreso");
+  elemento.style.display = "block";
+  elemento2.style.display = "none";
+  elemento3.style.display = "none";
 }
--- public/js/tincion.js
+++ public/js/tincion.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
   var selectedId;
-
-  getSelect();
-
   $("tbody").on("click", ".openModalBtn", function () {
     $("#myModal").css("display", "block");
     selectedId = $(this).data("id");
     $("#confirmacion").prop("checked", false);
-    $("#observacion").val();
-    console.log("ID seleccionado:", selectedId);
+    $("#observacion").val("");
   });
 
   $(".close").click(function () {
@@ -41,11 +37,17 @@ $(document).ready(function () {
       .then((data) => {
         if (data.success) {
           fetch("tincion/getData")
-            .then((response) => response.json())
+            .then((response) => {
+              response.json();
+              if (!response.json.success) {
+                $("#myModal").css("display", "none");
+                const tbody = $("tbody");
+                tbody.empty();
+              }
+            })
             .then((data) => {
               if (data) {
                 actualizarTabla(data);
-
                 $("#myModal").css("display", "none");
               } else {
                 alert("Error en la actualización: " + data.message);
@@ -59,6 +61,8 @@ $(document).ready(function () {
         }
       })
       .catch((error) => {
+        const tbody = $("tbody");
+        tbody.empty();
         console.error("Error en la solicitud Fetch: ", error);
       });
   });
@@ -86,179 +90,4 @@ $(document).ready(function () {
       tbody.append(tr);
     });
   }
-
-  $("#myFormTincion").on("submit", function (event) {
-    event.preventDefault();
-
-    var formData = {
-      id: selectedId,
-      confirmacion: $("#confirmacion").val(),
-      observacion: $("#observacion").val(),
-    };
-
-    fetch("tincion/updateTincion", {
-      method: "POST",
-      body: JSON.stringify(formData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          fetch("tincion/getData")
-            .then((response) => response.json())
-            .then((data) => {
-              if (data) {
-                actualizarTabla(data);
-                $("#myModal").css("display", "none");
-              } else {
-                alert("Error en la actualización: " + data.message);
-              }
-            })
-            .catch((error) => {
-              console.error("Error en la solicitud Fetch: ", error);
-            });
-        } else {
-          alert("Error en la actualización: " + data.message);
-        }
-      })
-      .catch((error) => {
-        console.error("Error en la solicitud Fetch: ", error);
-      });
-  });
-
-  function actualizarTabla(data) {
-    console.log(data);
-    const tbody = $("tbody");
-    tbody.empty();
-
-    $.each(data, function (i, row) {
-      $("#observacion").val(row.observacion);
-      $("#fecha").val(row.fecha);
-
-      const tr = $("<tr>");
-      tr.html(
-        `
-        <td> ${row.id} </td>
-        <td> ${row.examen_id} </td>
-        <td> ${row.confirmacion} </td>
-        <td> ${row.observacion} </td>
-        <td> ${row.fecha} </td>
-        <td> ${row.hora} </td>
-        <td>
-            <button data-id="${row.id}" type='button' class='btn btn-primary openModalBtn'>Diagnosticar</button>
-        </td>
-
-        `
-      );
-      tbody.append(tr);
-    });
-  }
-
-
-
-
-  $("#buscar").on("change", function (event) {
-    event.preventDefault();
-
-    var formData = {
-      id: $(this).val(),
-    };
-
-    fetch("tincion/buscarId", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          actualizarTabla(data);
-        } else {
-          alert("Error en la actualización: " + data.message);
-        }
-      })
-      .catch((error) => {
-        console.error("Error en la solicitud Fetch: ", error);
-      });
-  });
-
-
-
-
-
-
-  function getSelect() {
-    fetch("tincion/getData")
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          const select = $("select");
-          $.each(data, function (i, row) {
-            const option = $(
-              `<option data-test="${row.id}" class="obtenerID"> ${row.id} </option>`
-            );
-            select.append(option);
-          });
-        } else {
-          alert("Error en la actualización: " + data.message);
-        }
-      })
-      .catch((error) => {
-        console.error("Error en la solicitud Fetch: ", error);
-      });
-  }
-
-  $("#miAdmin").on("change", function (event) {
-    event.preventDefault();
-    var formData = {
-      id: $(this).find(":selected").data("test"),
-    };
-    fetch("tincion/buscarId", {
-      method: "POST",
-      body: JSON.stringify(formData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          actualizarTabla(data);
-        } else {
-          alert("Error en la actualización: " + data.message);
-        }
-      });
-    console.log("ID seleccionado:", $(this).find(":selected").data("test"));
-  });
-
-  $("#buscar").on("change", function (event) {
-    event.preventDefault();
-    var formData = {
-      id: $(this).val(),
-    };
-
-    fetch("tincion/buscarId", {
-      method: "POST",
-      body: JSON.stringify(formData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          //aqui ponen la funcion o llamada que quieren hacer  en este caso pueden usar este o modificarla
-          actualizarTabla(data);
-        } else {
-          alert("Error en la actualización: " + data.message);
-        }
-      })
-      .catch((error) => {
-        console.error("Error en la solicitud Fetch: ", error);
-      });
-  });
 });
