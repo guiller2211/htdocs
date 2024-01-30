b1035fd7ca7a73518ca923006f2a5b97757db246
Guillermo Araujo <guillermo.araujo@smithcommerce.com>
2024-01-29 22:39:41 -0300

    agregando logica de js aparte

--- App/Views/diagnostico/index.php
+++ App/Views/diagnostico/index.php
@@ -53,25 +53,25 @@
                         </tr>
                     </thead>
                     <tbody>
-                    <?php foreach ($examenes as $examen): ?>
-                    <tr>
-                        <td ><?php echo $examen["id"]; ?></td>
-                        <td><?php echo $examen["diagnostico_codigo"]; ?></td>
-                        <td><?php echo $examen["rut"]; ?></td>
-                        <td><?php echo $examen["nombre"]; ?></td>
-                        <td><?php echo $examen["apPat"]; ?></td>
-                        <td><?php echo $examen["apMat"]; ?></td>
-                        <td><?php echo $examen["confirmacion"] == 1 ? "Confirmado":"No Confirmado"; ?></td>
-                        <td><?php echo $examen["observacion"]; ?></td>
-                        <td>
-                                <div class="row gap-1">
-                                    <div class="col">
-                                        <a href="#" onclick="openModal(<?= $examen['id'];?>)">Diagnosticar</a>
+                        <?php foreach ($examenes as $examen) : ?>
+                            <tr>
+                                <td><?php echo $examen["id"]; ?></td>
+                                <td><?php echo $examen["diagnostico_codigo"]; ?></td>
+                                <td><?php echo $examen["rut"]; ?></td>
+                                <td><?php echo $examen["nombre"]; ?></td>
+                                <td><?php echo $examen["apPat"]; ?></td>
+                                <td><?php echo $examen["apMat"]; ?></td>
+                                <td><?php echo $examen["confirmacion"] == 1 ? "Confirmado" : "No Confirmado"; ?></td>
+                                <td><?php echo $examen["observacion"]; ?></td>
+                                <td>
+                                    <div class="row gap-1">
+                                        <div class="col">
+                                            <a href="#" onclick="openModal(<?= $examen['id']; ?>)">Diagnosticar</a>
+                                        </div>
                                     </div>
-                                </div>
-                            </td>
-                    </tr>
-                <?php endforeach;?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
             </div>
@@ -87,9 +87,9 @@
 
                 <label for="modEstado">Nuevo Estado:</label>
                 <select id="modEstado" name="modEstado" required>
-                <?php foreach ($codigosDiagnostico as $cod): ?>
-                    <option value="<?= $cod['codigo']; ?>"><?= $cod['descripcion']; ?></option>
-                <?php endforeach;?>
+                    <?php foreach ($codigosDiagnostico as $cod) : ?>
+                        <option value="<?= $cod['codigo']; ?>"><?= $cod['descripcion']; ?></option>
+                    <?php endforeach; ?>
                 </select>
 
 
@@ -100,83 +100,7 @@
     </div>
 
     </div>
-    <script>
-        // Funciones para mostrar y ocultar el modal
-        function openModal(examId) {
-            //Cargar la información del examen correspondiente en el modal
-            document.getElementById("myModal").style.display = "flex";
-            document.getElementById("idExamen").value = examId;
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
 
-        }
-
-
-        function closeModal() {
-            document.getElementById("myModal").style.display = "none";
-        }
-
-        // Función para procesar la actualización del examen
-        function updateExam() {
-            // Aquí puedes agregar lógica para actualizar el examen en la base de datos
-            // y cerrar el modal después de la actualización
-            var codigo_diagnostico = document.getElementById("modEstado").value;
-            var idExamen = document.getElementById("idExamen").value;
-            
-            // Configurar los datos que se enviarán en la solicitud
-
-            var data = {
-            id: idExamen,
-            codigoDiagnostico: codigo_diagnostico
-            };
-            
-            // Realizar la llamada Fetch
-            fetch("diagnostico/actualizarEstadoDiagnostico", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-                // Puedes agregar más encabezados según sea necesario
-            },
-            body: JSON.stringify(data)
-            })
-            .then(response => response.json())
-            .then(data => {
-                // Hacer algo con la respuesta del servidor
-                if(data.success){
-                    //Recargar la pagina
-                    alert("Registro actualizado correctamente");
-                    window.location.reload();
-
-                }else{
-                    alert("Ocurrio un error");
-                }
-                console.log(data);
-            })
-            .catch(error => {
-                // Manejar errores en la llamada Fetch
-                console.error('Error en la llamada Fetch:');
-            });
-
-            closeModal();
-        }
-        // Cerrar el modal si se hace clic fuera del contenido
-        window.onclick = function(event) {
-            if (event.target == document.getElementById("myModal")) {
-                closeModal();
-            }
-            if (event.target == document.getElementById("muestrasModal")) {
-                closeMuestrasModal();
-            }
-        };
-
-        // Funciones para mostrar y ocultar el modal de ver muestras
-
-        function openMuestrasModal(examId) {
-            // Aquí puedes cargar la información de las muestras correspondientes en el modal de ver muestras
-            document.getElementById("muestrasModal").style.display = "flex";
-        }
-
-        function closeMuestrasModal() {
-            document.getElementById("muestrasModal").style.display = "none";
-        }
-    </script>
+    <script src="../../../../public/js/diagnostico.js"></script>
 </body>
-</html>
